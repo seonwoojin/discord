55dcb52e5fc11cf761528ae012a76a4601319b29
Seonwoo <wlstjsdn12@gmail.com>
2023-03-22 16:07:21 +0900

    .

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: 32767 });
-const CHANNEL_ID = "1083750618503589950"; // 가져올 채널의 ID
-
-client.once("ready", () => {
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [513],
+});
+const CHANNEL_ID = "940359293302108211"; // 가져올 채널의 ID
+require("dotenv").config();
+client.once("ready", async () => {
   console.log("봇이 준비되었습니다.");
-  const channel = client.channels.cache.get(CHANNEL_ID);
-  if (!channel) {
-    console.log(`채널을 찾을 수 없습니다: ${CHANNEL_ID}`);
-    return;
-  }
-  channel
-    .fetch()
-    .then((channel) => console.log(channel))
-    .catch(console.error);
+  const channel = await client.channels.fetch(CHANNEL_ID);
+  console.log(channel);
+  // if (!channel) {
+  //   console.log(`채널을 찾을 수 없습니다: ${CHANNEL_ID}`);
+  //   return;
+  // }
+  // channel
+  //   .fetch()
+  //   .then((channel) => console.log(channel))
+  //   .catch(console.error);
 });
 
-client.login(
-  "MTA4MzcyMTQyNDEwMDkzNzgyOQ.GV6PMq.2Uqs7AUHwmdUx0_FYL0MEEf9CNcjAMx3dda4Ss"
-);
+client.login(process.env.TOKEN);
