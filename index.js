3d0b1454d28b051502187c3056025a03748a68f6
Seonwoo <wlstjsdn12@gmail.com>
2023-03-22 13:57:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ ws.on("message", async function incoming(data) {
       d.guild_id === "938836025659232327" &&
       (d.channel_id === "940359293302108211" ||
         d.channel_id === "1062253413854871573" ||
-        d.channel_id === "1072410249451020319")
+        d.channel_id === "1072410249451020319") &&
+      t !== "MESSAGE_REACTION_ADD"
     ) {
       console.log(t, JSON.stringify(d));
       await axios.post("https://www.bluetags.app/api/admin/create-rawData", {
